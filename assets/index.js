2b940713f93f7814ecec1c9a75b0606abcb6e1b4
Jesse Emerson <jesseemerson7@gmail.com>
2023-08-11 00:58:25 -0400

    working on timing log

--- assets/index.js
+++ assets/index.js
@@ -4,15 +4,20 @@ const pattern = [];
 const startGame = (firstButton) => {
   pattern.push(parseInt(firstButton));
   console.log(...pattern);
+  //after first push a new random number is selected and shown to the user
+  pattern.push(randomInt());
   showPattern();
 };
 
 const showPattern = () => {
-  let randomInt = Math.floor(Math.random() * 4) + 1
-  pattern.push(randomInt);
-  
+  let index = 0;
+
+  if (index < pattern.length) {
+    console.log(pattern[index]);
+    //!TODO: working here to get 1 sec delay on logging out pattern with new value at the end
+  }
 };
 
+const randomInt = () => Math.floor(Math.random() * 4) + 1;
+
 gameBoard.click((e) => startGame(e.target.getAttribute("data")));
-
-

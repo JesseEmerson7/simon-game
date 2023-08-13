ae303ac506b830567933f1f1558a617398d31db1
Jesse Emerson <jesseemerson7@gmail.com>
2023-08-13 00:22:51 -0400

    changing timing func

--- assets/index.js
+++ assets/index.js
@@ -2,22 +2,28 @@ const gameBoard = $("#input-div");
 const pattern = [];
 
 const startGame = (firstButton) => {
-  pattern.push(parseInt(firstButton));
-  console.log(...pattern);
+  if([pattern.length === 0]){
+    pattern.push(parseInt(firstButton));
+  }
+  
+  // console.log(...pattern);
   //after first push a new random number is selected and shown to the user
   pattern.push(randomInt());
-  showPattern();
+  setTimeout(showPattern,1000);
 };
 
-const showPattern = () => {
-  let index = 0;
-
-  if (index < pattern.length) {
-    console.log(pattern[index]);
-    //!TODO: working here to get 1 sec delay on logging out pattern with new value at the end
-  }
+const showPattern = (arrIndex = 0) => {
+  if (arrIndex < pattern.length) {
+    console.log(pattern[arrIndex]);
+    setTimeout(() => {
+      showPattern(arrIndex + 1);
+    },1000)
+  
+};
 };
 
 const randomInt = () => Math.floor(Math.random() * 4) + 1;
 
-gameBoard.click((e) => startGame(e.target.getAttribute("data")));
+  gameBoard.click((e) => startGame(e.target.getAttribute("data")));
+
+
